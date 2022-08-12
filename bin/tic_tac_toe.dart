e0d56ca9d5005a8359b0882c7b948170181f05b0
Pazh-IT <mhwmdyaghubkhani@gmail.com>
2022-08-13 00:48:01 +0430

    Initial commit

---
+++ bin/tic_tac_toe.dart
@@ -0,0 +1,5 @@
+import 'package:tic_tac_toe/tic_tac_toe.dart' as tic_tac_toe;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tic_tac_toe.calculate()}!');
+}
---
+++ test/tic_tac_toe_test.dart
@@ -0,0 +1,8 @@
+import 'package:tic_tac_toe/tic_tac_toe.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
