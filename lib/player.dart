17b9cf5938d22f7f7ba2117be0b49a095e662856
Pazh-IT <mhwmdyaghubkhani@gmail.com>
2022-08-13 14:50:18 +0430

    Opponent player getter on enum Player

--- lib/player.dart
+++ lib/player.dart
@@ -2,4 +2,10 @@ enum Player {
   none,
   first,
   second;
+
+  Player get opponent {
+    if (this == Player.first) return Player.second;
+    if (this == Player.second) return Player.first;
+    return Player.none;
+  }
 }
