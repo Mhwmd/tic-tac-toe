1a74f71aaa1ab9c78b49423b67b21a1a3ba81ab8
Pazh-IT <mhwmdyaghubkhani@gmail.com>
2022-08-14 01:18:44 +0430

    sort functions top to down

--- lib/game_logic.dart
+++ lib/game_logic.dart
@@ -1,38 +1,29 @@
 import 'package:tic_tac_toe/extensions.dart';
 import 'package:tic_tac_toe/tic_tac_toe.dart';
 
-Board copyBoard(Board board) => board.map(BoardRow.from).toList();
+Game changeTurn(Game game) => game.copyWith(turn: game.turn.opponent);
+
+Game checkGameOver(Game state) {
+  if (checkWin(state.board, state.turn)) return handleWin(state);
+  if (checkDraw(state.board)) return handleDraw(state);
+  return handleRunning(state);
+}
+
+bool checkWin(Board board, Player player) {
+  return checkRowsForWin(board, player) ||
+      checkRowsForWin(transposeBoard(board), player) ||
+      checkWinDiagonalSides(board, player);
+}
+
+bool checkRowsForWin(Board board, Player player) => board.any((row) => checkRowForWin(row, player));
+
+bool checkRowForWin(BoardRow row, Player player) => row.every((val) => val == player);
 
 Board transposeBoard(Board board) => zip(board).toList();
 
-Iterable<Position> getBoardPositions(Board board) {
-  return board
-      .mapIndexed((rowIndex, row) => row.mapIndexed((colIndex, _) => Position(rowIndex, colIndex)))
-      .expand((position) => position);
+bool checkWinDiagonalSides(Board board, Player player) {
+  return checkRowForWin(getLeftDiagonal(board), player) || checkRowForWin(getRightDiagonal(board), player);
 }
-
-Board occupyPosition(Board board, Position position, Player player) {
-  var newBoard = copyBoard(board);
-  newBoard[position.x][position.y] = player;
-  return newBoard;
-}
-
-Board genMove(Game game, Position position) => occupyPosition(game.board, position, game.turn);
-
-bool positionOccupied(Board board, Position position, Player player) => board[position.x][position.y] == player;
-
-bool positionUnoccupied(Board board, Position position) => positionOccupied(board, position, Player.none);
-
-bool occupiedByFirstPlayer(Board board, Position position) => positionOccupied(board, position, Player.first);
-
-bool occupiedBySecondPlayer(Board board, Position position) => positionOccupied(board, position, Player.second);
-
-List<Position> getOccupiedPositions(Board board, bool Function(Board board, Position position) test) {
-  Iterable<Position> positions = getBoardPositions(board);
-  return positions.where((position) => test(board, position)).toList();
-}
-
-List<Position> getLegalMoves(Board board) => getOccupiedPositions(board, positionUnoccupied);
 
 BoardRow getLeftDiagonal(Board board) => getDiagonal(board, (row, i) => row[i]);
 
@@ -46,38 +37,47 @@ BoardRow getDiagonal(Board board, Player Function(BoardRow row, int i) operation
   });
 }
 
-bool checkRowForUnoccupied(BoardRow row) => row.any((val) => val == Player.none);
-
-bool checkBoardForUnoccupied(Board board) => board.any(checkRowForUnoccupied);
+Game handleWin(Game game) => game.copyWith(state: GameOver(game.turn));
 
 bool checkDraw(Board board) => !checkBoardForUnoccupied(board);
 
-bool checkRowForWin(BoardRow row, Player player) => row.every((val) => val == player);
+bool checkBoardForUnoccupied(Board board) => board.any(checkRowForUnoccupied);
 
-bool checkRowsForWin(Board board, Player player) => board.any((row) => checkRowForWin(row, player));
-
-bool checkWinDiagonalSides(Board board, Player player) {
-  return checkRowForWin(getLeftDiagonal(board), player) || checkRowForWin(getRightDiagonal(board), player);
-}
-
-bool checkWin(Board board, Player player) {
-  return checkRowsForWin(board, player) ||
-      checkRowsForWin(transposeBoard(board), player) ||
-      checkWinDiagonalSides(board, player);
-}
-
-Game changeTurn(Game game) => game.copyWith(turn: game.turn.opponent);
-
-Game runMove(Game game, Position position) => game.copyWith(board: genMove(game, position));
-
-Game checkGameOver(Game state) {
-  if (checkWin(state.board, state.turn)) return handleWin(state);
-  if (checkDraw(state.board)) return handleDraw(state);
-  return handleRunning(state);
-}
-
-Game handleWin(Game game) => game.copyWith(state: GameOver(game.turn));
+bool checkRowForUnoccupied(BoardRow row) => row.any((val) => val == Player.none);
 
 Game handleDraw(Game game) => game.copyWith(state: const GameOver(Player.none));
 
 Game handleRunning(Game game) => game.copyWith(state: const Running());
+
+Game runMove(Game game, Position position) => game.copyWith(board: genMove(game, position));
+
+Board genMove(Game game, Position position) => occupyPosition(game.board, position, game.turn);
+
+Board occupyPosition(Board board, Position position, Player player) {
+  var newBoard = copyBoard(board);
+  newBoard[position.x][position.y] = player;
+  return newBoard;
+}
+
+List<Position> getLegalMoves(Board board) => getOccupiedPositions(board, positionUnoccupied);
+
+bool positionUnoccupied(Board board, Position position) => positionOccupied(board, position, Player.none);
+
+bool occupiedByFirstPlayer(Board board, Position position) => positionOccupied(board, position, Player.first);
+
+bool occupiedBySecondPlayer(Board board, Position position) => positionOccupied(board, position, Player.second);
+
+bool positionOccupied(Board board, Position position, Player player) => board[position.x][position.y] == player;
+
+List<Position> getOccupiedPositions(Board board, bool Function(Board board, Position position) test) {
+  Iterable<Position> positions = getBoardPositions(board);
+  return positions.where((position) => test(board, position)).toList();
+}
+
+Iterable<Position> getBoardPositions(Board board) {
+  return board
+      .mapIndexed((rowIndex, row) => row.mapIndexed((colIndex, _) => Position(rowIndex, colIndex)))
+      .expand((position) => position);
+}
+
+Board copyBoard(Board board) => board.map(BoardRow.from).toList();
