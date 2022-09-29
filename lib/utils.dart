a42e9a9ea14219929aa6b52d4886c298cba72b4a
Pazh-IT <mhwmdyaghubkhani@gmail.com>
2022-09-29 07:10:20 +0330

    random choice for ai

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,14 @@
+import 'dart:math';
+
 Iterable<List<T>> zip<T>(Iterable<Iterable<T>> iterables) sync* {
   if (iterables.isEmpty) return;
   final iterators = iterables.map((e) => e.iterator).toList(growable: false);
   while (iterators.every((e) => e.moveNext())) {
     yield iterators.map((e) => e.current).toList(growable: false);
   }
+}
+
+T randomChoice<T>(Iterable<T> options) {
+  int choice = Random().nextInt(options.length);
+  return options.elementAt(choice);
 }
